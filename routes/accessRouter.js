a286c85c3102b10117ef31094a2e5a314266f65a
nurvandwi <nurvandwi20@gmail.com>
2021-03-24 00:26:45 +0700

    update registrasi and login user

---
+++ routes/accessRouter.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const UserController = require('../controllers/UserController.js')
+
+router.get('/register', UserController.registerForm)
+router.post('/register', UserController.registerAdd)
+router.get('/login', UserController.login)
+router.post('/login', UserController.loginPost)
+
+
+module.exports = router
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const UserController = require('../controllers/UserController.js')
+const MenuUserController = require('../controllers/MenuUserController.js')
 
-router.get('/register', UserController.registerForm)
-router.post('/register', UserController.registerAdd)
-router.get('/login', UserController.login)
-router.post('/login', UserController.loginPost)
+router.get('/menuuser', MenuUserController.findAll)
+
 
 
 module.exports = router
